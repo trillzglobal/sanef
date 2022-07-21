bdfea0b014716def51af436c12f5b6a03b9b8871
Drame Success <d.ije95@gmail.com>
2022-07-21 14:59:16 +0100

    Fixed Cal

--- src/Sanef.php
+++ src/Sanef.php
@@ -1,12 +1,15 @@
 <?php
+
 namespace Sanef;
+
 use \GuzzleHttp\{
     Client,
     Exception\ClientException,
     Exception\RequestException
 };
 
-class Sanef extends Validate {
+class Sanef extends Validate
+{
     public $superAgentCode;
     public $gpgPayload;
     public $gpgKey;
@@ -15,9 +18,9 @@ class Sanef extends Validate {
     public $base_url;
     public $encryptedPayload;
     public $checked_data;
-    
 
-    public function __construct( string $baseURL, string $superAgentCode, string $gpgKey, string $gpgPassphrase="")
+
+    public function __construct(string $baseURL, string $superAgentCode, string $gpgKey, string $gpgPassphrase = "")
     {
         $this->superAgentCode = $superAgentCode;
         $this->gpgKey = $gpgKey;
@@ -25,51 +28,54 @@ class Sanef extends Validate {
         $this->base_url = $baseURL;
     }
 
-    private function encrypt(){
+    private function encrypt()
+    {
         $gpg = new gnupg();
-        $gpg -> addencryptkey($this->gpgKey);
-        return $gpg -> encrypt($this->payload);
+        $gpg->addencryptkey($this->gpgKey);
+        return $gpg->encrypt($this->payload);
     }
 
-    private function decrypt($gpgResponse){
+    private function decrypt($gpgResponse)
+    {
         $gpg = new gnupg();
         $gpg->adddecryptkey($this->gpgKey, $this->gpgPassphrase);
-        $plain =$gpg->decrypt($gpgResponse);
+        $plain = $gpg->decrypt($gpgResponse);
         return $plain;
     }
 
-    public function call(){
-        
+    public function call()
+    {
+
         $client = new Client();
-        try{
+        try {
 
-            if(!empty($this->gpgPayload)){
+            if (!empty($this->gpgPayload)) {
 
-                $data =  $client->request('POST',$this->base_url.$this->endpoint,[
-                    'headers'=>["superAgentCode"=>$this->superAgentCode, "Content-Type"=>"application/json"],
-                    'json'=>$this->gpgPayload
+                $request =  $client->request('POST', $this->base_url . $this->endpoint, [
+                    'headers' => ["ClientID" => $this->superAgentCode, "Content-Type" => "application/json"],
+                    'body' => $this->gpgPayload,
+                    'debug' => true
                 ]);
-            }
-            else{
-                $data =  $client->request('GET',$this->base_url.$this->endpoint,[
-                    'headers'=>["ClientID"=>$this->superAgentCode, "Content-Type"=>"application/json"]
+            } else {
+                $request =  $client->request('GET', $this->base_url . $this->endpoint, [
+                    'headers' => ["ClientID" => $this->superAgentCode, "Content-Type" => "application/json"]
                 ]);
             }
             //Body of response
-            $data =(string) $data->getBody(true);
+            $data = (string) $request->getBody(true);
             $data = json_decode($data, true);
             $data = json_decode($this->decrypt($data["data"]), true);
-            $response = json_encode(["data"=>$data, "status"=>"success"]);
-        }catch(RequestException $e){
+            $response = json_encode(["data" => $data, "status" => "success"]);
+        } catch (RequestException $e) {
 
-            if($e->hasResponse())
-                $data =(string) $e->getResponse()->getBody(true);
+            if ($e->hasResponse())
+                $data = (string) $e->getResponse()->getBody(true);
             $data = json_decode($data, true);
-            if(!empty($data["data"])){
+            if (!empty($data["data"])) {
                 $data = json_decode($this->decrypt($data["data"]), true);
             }
             // var_dump($e);
-            $response =json_encode(["data"=>$data ? $data : $e, "status"=>"failed"]);
+            $response = json_encode(["data" => $data ? $data : $e, "status" => "failed"]);
         }
 
         return $response;
@@ -78,78 +84,92 @@ class Sanef extends Validate {
     /*
     Create New Agent
     */
-    private function report($data){
-        if($data["status"] == "error"){
+    private function report($data)
+    {
+        if ($data["status"] == "error") {
             return json_encode($data);
         }
         $this->payload = json_encode($data['details']);
         // $this->gpgPayload = json_encode(["data"=>$this->encrypt()]);
 
-       // return $this->call();
+        // return $this->call();
     }
 
-    public function createAgent(array $data){
+    public function createAgent(array $data)
+    {
         $this->endpoint = "api/v1/agents/create";
         $data = $this->checkCreateAgent($data);
-      return $this->report($data);
+        return $this->report($data);
     }
 
-    public function updateAgent(array $data){
+    public function updateAgent(array $data)
+    {
         $data = $this->checkUpdateAgent($data);
         return $this->report($data);
     }
 
-    public function agentDetails(array $data){
+    public function agentDetails(array $data)
+    {
         $data = $this->checkAgentDetails($data);
         return $this->report($data);
     }
 
-    public function createWallet(array $data){
+    public function createWallet(array $data)
+    {
         $data = $this->checkCreateWallet($data);
         return $this->report($data);
     }
 
-    public function createAccount(array $data){
+    public function createAccount(array $data)
+    {
         $data = $this->checkCreateAccount($data);
         return $this->report($data);
     }
-    
-    public function validateCashCode(array $data){
+
+    public function validateCashCode(array $data)
+    {
         $data = $this->checkValidateCashCode($data);
         return $this->report($data);
     }
 
-    public function approveCashCode(array $data){
+    public function approveCashCode(array $data)
+    {
         $data = $this->checkApproveCashCode($data);
         return $this->report($data);
     }
 
-    public function requeryCashCodeStatus(array $data){
+    public function requeryCashCodeStatus(array $data)
+    {
         $data = $this->checkRequeryCashCodeStatus($data);
         return $this->report($data);
     }
 
-    public function log(array $data){
+    public function log(array $data)
+    {
         $data = $this->checkLog($data);
         return $this->report($data);
     }
 
-    public function history(array $data){
+    public function history(array $data)
+    {
         $data = $this->checkHistory($data);
         return $this->report($data);
     }
 
-    public function cardRequestSendOtp (array $data){
+    public function cardRequestSendOtp(array $data)
+    {
         $data = $this->checkCardRequestSendOtp($data);
         return $this->report($data);
     }
 
-    public function cardRequestLinkCard(array $data){
+    public function cardRequestLinkCard(array $data)
+    {
         $data = $this->checkcardRequestLinkCard($data);
         return $this->report($data);
     }
 
-    public function queryAccountOpeningStatus(array $data){
+    public function queryAccountOpeningStatus(array $data)
+    {
         $data = $this->checkQueryAccountOpeningStatus($data);
         return $this->report($data);
     }
